d7d2a795ebd11bb34e3bd75fc1c96d88f128cf7c
Proflylab <proflylab@protonmail.com>
2018-03-21 14:42:39 +0700

    First Release

---
+++ Lefiya/frmMain.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Lefiya
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.lstData = new System.Windows.Forms.ListView();
+            this.colFile = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colDirectory = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnImport = new MetroFramework.Controls.MetroButton();
+            this.btnExport = new MetroFramework.Controls.MetroButton();
+            this.lblVersion = new MetroFramework.Controls.MetroLabel();
+            this.btnClear = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // lstData
+            // 
+            this.lstData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstData.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colFile,
+            this.colDirectory});
+            this.lstData.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstData.FullRowSelect = true;
+            this.lstData.Location = new System.Drawing.Point(20, 70);
+            this.lstData.MultiSelect = false;
+            this.lstData.Name = "lstData";
+            this.lstData.Size = new System.Drawing.Size(680, 355);
+            this.lstData.TabIndex = 0;
+            this.lstData.UseCompatibleStateImageBehavior = false;
+            this.lstData.View = System.Windows.Forms.View.Details;
+            // 
+            // colFile
+            // 
+            this.colFile.Text = "File";
+            this.colFile.Width = 650;
+            // 
+            // colDirectory
+            // 
+            this.colDirectory.Text = "Directory";
+            this.colDirectory.Width = 0;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnImport.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnImport.Location = new System.Drawing.Point(463, 438);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 23);
+            this.btnImport.TabIndex = 1;
+            this.btnImport.Text = "IMPORT";
+            this.btnImport.UseSelectable = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnExport.Location = new System.Drawing.Point(544, 438);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "EXPORT";
+            this.btnExport.UseSelectable = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(15, 441);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(15, 19);
+            this.lblVersion.TabIndex = 3;
+            this.lblVersion.Text = "?";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClear.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.btnClear.Location = new System.Drawing.Point(625, 438);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "CLEAR";
+            this.btnClear.UseSelectable = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = MetroFramework.Forms.MetroFormBorderStyle.FixedSingle;
+            this.ClientSize = new System.Drawing.Size(720, 480);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.lstData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(720, 480);
+            this.Name = "frmMain";
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
+            this.Style = MetroFramework.MetroColorStyle.Green;
+            this.Text = "KONAYUKI LEFIYA";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lstData;
+        private System.Windows.Forms.ColumnHeader colFile;
+        private System.Windows.Forms.ColumnHeader colDirectory;
+        private MetroFramework.Controls.MetroButton btnImport;
+        private MetroFramework.Controls.MetroButton btnExport;
+        private MetroFramework.Controls.MetroLabel lblVersion;
+        private MetroFramework.Controls.MetroButton btnClear;
+    }
+}
+
---
+++ Lefiya/frmMain.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Text.RegularExpressions;
+
+using MetroFramework;
+using MetroFramework.Forms;
+using System.Reflection;
+
+namespace Lefiya
+{
+    public partial class frmMain : MetroForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            lblVersion.Text = $"{Assembly.GetExecutingAssembly().GetName().Version.ToString()}";
+        }
+        private void ImportFiles(string[] files)
+        {
+            foreach (var file in files)
+            {
+                if (new string[] { ".ass", ".ssa" }.Contains(Path.GetExtension(file).ToLower()))
+                    lstData.Items.Add(new ListViewItem(new string[] { Path.GetFileName(file), Path.GetDirectoryName(file) }));
+            }
+        }
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            var ofdImport = new OpenFileDialog
+            {
+                Title = "IMPORT",
+                Filter = "Advanced SubStation Alpha (*.ass,*.ssa)|*.ass;*.ssa",
+                Multiselect = true
+            };
+
+            if (ofdImport.ShowDialog(this) == DialogResult.OK)
+            {
+                ImportFiles(ofdImport.FileNames);
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            if (lstData.Items.Count > 0)
+            {
+                var fbdExport = new FolderBrowserDialog
+                {
+                    Description = "Browse folder to Export your Files."
+                };
+
+                if (fbdExport.ShowDialog(this) == DialogResult.OK)
+                {
+                    foreach (ListViewItem file in lstData.Items)
+                    {
+                        var path = Path.Combine(file.SubItems[1].Text, file.SubItems[0].Text);
+
+                        if (File.Exists(path))
+                        {
+                            var lines = File.ReadAllLines(path, Encoding.UTF8);
+                            for (int line = 0; line < lines.Length; line++)
+                            {
+                                var idx = Regex.Match(lines[line], "Dialogue:(?<Layer>.*?),(?<Start>.*?),(?<End>.*?),(?<Style>.*?),(?<Name>.*?),(?<MarginL>.*?),(?<MarginR>.*?),(?<MarginV>.*?),(?<Effect>.*?),(?<Text>.*)").Groups["Text"].Index;
+                                var len = Regex.Match(lines[line], "Dialogue:(?<Layer>.*?),(?<Start>.*?),(?<End>.*?),(?<Style>.*?),(?<Name>.*?),(?<MarginL>.*?),(?<MarginR>.*?),(?<MarginV>.*?),(?<Effect>.*?),(?<Text>.*)").Groups["Text"].Value.Length;
+                                if (lines[line].Contains("Dialogue:"))
+                                {
+                                    if (lines[line].Contains("{"))
+                                    {
+                                        if (lines[line][lines[line].Length - 1] != '}')
+                                        {
+                                            lines[line] = lines[line] + '}';
+                                        }
+                                        for (int j = lines[line].Length - 1; j > -1; j--)
+                                        {
+                                            if (lines[line][j] == '{')
+                                            {
+                                                if (lines[line][j - 1] != '}' && j - 1 > idx)
+                                                {
+                                                    lines[line] = lines[line].Substring(0, j) + '}' + lines[line].Substring(j);
+                                                }
+                                            }
+                                            if (lines[line][j] == '}')
+                                            {
+                                                if (j + 1 < lines[line].Length && lines[line][j + 1] != '{')
+                                                {
+                                                    lines[line] = lines[line].Substring(0, j + 1) + '{' + lines[line].Substring(j + 1);
+                                                }
+                                            }
+                                        }
+                                        if (lines[line][idx] != '{')
+                                        {
+                                            lines[line] = lines[line].Substring(0, idx) + "{" + lines[line].Substring(idx);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (len != 0)
+                                        {
+                                            lines[line] = lines[line].Substring(0, idx) + "{" + lines[line].Substring(idx) + "}";
+                                        }
+                                    }
+                                }
+                            }
+                            File.WriteAllLines(Path.Combine(fbdExport.SelectedPath, Path.GetFileNameWithoutExtension(file.SubItems[0].Text)) + "_CLEAN.ass", lines, Encoding.UTF8);
+                            MetroMessageBox.Show(this, "Complete", "MsgBox", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstData.Items.Clear();
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            ImportFiles(e.Data.GetData(DataFormats.FileDrop) as string[]);
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+    }
+}
